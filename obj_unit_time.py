1ebf43aae5492d8bc5e18235e51312433e9f8e29
kasobi <nicholaskasbar@gmail.com>
2026-08-06 23:32:41 -0700

    new branch for physics sim

---
+++ obj_unit_time.py
@@ -0,0 +1,39 @@
+import pygame
+
+
+
+
+class UnitSpaceTime():
+    def __init__(self):
+        #past
+        self.top = None
+        self.bottom = None
+        self.left = None
+        self.right = None
+        self.forward = None
+        self.backward = None
+
+        #future / present
+        self.present = None
+
+    def collide():
+        top_to_bottom_vector = []
+        bottom_to_top_vector = []
+        left_to_right_vector = []
+        right_to_left_vector = []
+        forward_to_backward_vector = []
+        backward_to_forward_vector = []
+
+    def top_to_bottom_vector(self):
+        return 
+
+
+
+
+
+
+class UnitSphere():
+    def __init__(self, x, y, z, ):
+
+
+    
